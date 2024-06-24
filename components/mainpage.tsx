a2ab6dd6d6fafcb5bbacc53da2835d0356cdcf84
Mihir Vador <mihirvador@Mihirs-MacBook-Air-2.local>
2024-06-24 14:06:25 -0700

    fixed box shadow on mobile

--- components/mainpage.tsx
+++ components/mainpage.tsx
@@ -97,7 +97,7 @@ export default function MainPage() {
               />
             ))}
           </div>
-          <div className="mt-10">
+          <div className="mt-10 mb-4">
             <h2 className="text-3xl font-bold text-center">Projects</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
               {projects.map((project, index) => (
