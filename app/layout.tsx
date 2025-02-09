a6d1febe57874abe033e2afc0a558b2851fa0ecd
Mihir Vador <mihirrocks9999@gmail.com>
2025-02-08 21:14:51 -0800

    fixed bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import type React from "react" // Added import for React
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Your Name - ML Engineer & Roboticist",
-  description: "Personal portfolio of an Infrastructure ML Engineer and Roboticist",
+  title: "Mihir Vador - Infrastructure & Machine Learning & Robotics",
+  description: "Personal portfolio of Mihir Vador",
 }
 
 export default function RootLayout({
